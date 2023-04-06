17d8221d633e29bd335eb5d8cd3ca37c03aa2197
fjgao <fjgao@bu.edu>
2023-04-05 20:17:45 -0400

    express uses react build as a static page

--- muvie/routes/users.js
+++ muvie/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.use(express.static(path.join('.','muvieview','build')));
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
