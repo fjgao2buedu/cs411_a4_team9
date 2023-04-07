59621aec8cad64e8eafb8287b4957d7b130812fe
zachmfishman <103368814+zachmfishman@users.noreply.github.com>
2023-04-07 03:15:35 -0400

    Update App.js

--- muvie/muvieview/src/App.js
+++ muvie/muvieview/src/App.js
@@ -47,7 +47,6 @@ function App() {
   const renderArtists = () => {
     return artists.map(artist => (
       <div key={artist.id}>
-        {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
         {artist.name}
       </div>
     ))
