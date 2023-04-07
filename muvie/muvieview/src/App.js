aec09e974fe45c9480ecbfe4440dc86ef7dc633f
lroxanne <123195819+lroxanne@users.noreply.github.com>
2023-04-06 21:45:03 -0400

    Update App.js

--- muvie/muvieview/src/App.js
+++ muvie/muvieview/src/App.js
@@ -1,39 +1,65 @@
-import { useState } from "react";
-import "./App.css";
-import { Grid, Typography } from "@mui/material";
-import SearchBar from "./components/SearchBar";
-import RecordTable from "./components/RecordTable";
-
+import './App.css';
+import {useEffect, useState} from 'react';
+import fetch from 'node-fetch';
 function App() {
-  const [content, setContent] = useState([]);
-  const callback = (content) => {
-    setContent(content);
-  };
+  const CLIENT_ID = "3f958fdf5b01422690ef323614cca86c"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await fetch("https://api.spotify.com/v1/search", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        params: {
+            q: searchKey,
+            type: "artist"
+        }
+    })
+
+    setArtists(data.artists.items)
+}
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+  
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+  const renderArtists = () => {
+    return artists.map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+  }
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Grid container spacing={2} style={{ padding: "1rem" }}>
-        <Grid xs={12} container alignItems="center" justifyContent="center">
-          <Typography variant="h2" gutterBottom>
-            Muvie Prototype
-          </Typography>
-        </Grid>
-        <Grid xs={12} md={4}>
-          <Typography variant="h4" gutterBottom>
-            Search
-          </Typography>
-          <div style={{ width: "100%" }}>
-            <SearchBar parentCallback={callback}></SearchBar>
-          </div>
-        </Grid>
-        <Grid xs={12} md={8}>
-          <Typography variant="h4" gutterBottom>
-            Results
-          </Typography>
-          <div style={{ height: 600, width: "100%" }}>
-            <RecordTable data={content} />
-          </div>
-        </Grid>
-      </Grid>
+    <div className="App">
+      <header className="App-header">
+        <h1>Spotify React</h1>
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+            to Spotify</a>
+          : <button onClick={logout}>Logout</button>}
+      </header>
     </div>
   );
 }
